15d651bd9d43da4b0e4202e8805493141636653d
unknown-user <unknown-user@fe80::4483:cb43:e9b4:5f86%12>
2014-05-10 19:47:09 +0200

    Se ha añadido el panel de activacion del sonido

--- Assets/Scripts/VolumeManager.cs
+++ Assets/Scripts/VolumeManager.cs
@@ -5,13 +5,19 @@ public class VolumeManager : MonoBehaviour
 {
 
 		UISlider slider;
+		public UIToggle soundToggle;
 	
 		void Awake ()
 		{
 				//cogemos el slider
 				slider = GetComponent<UISlider> ();
 				// set the slider's value to last saved value 
-				slider.value = NGUITools.soundVolume; 
+				slider.value = NGUITools.soundVolume;
+
+				// si el volumen es 0, desactivamos el check
+				if (NGUITools.soundVolume == 0)
+						soundToggle.value = false;
+				
 		}
 
 		// Use this for initialization
@@ -32,5 +38,16 @@ public class VolumeManager : MonoBehaviour
 				NGUITools.soundVolume = UISlider.current.value;
 				//Change the game audiolistener's volume
 				AudioListener.volume = UISlider.current.value;
+		}
+
+		public void OnSoundToggle ()
+		{
+				float newVolume = 0;
+				//si el boton esta checkeado, ponemos el slide a su valor
+				if (UIToggle.current.value)
+						newVolume = slider.value;
+				AudioListener.volume = newVolume;
+				NGUITools.soundVolume = newVolume;
+		 
 		}
 }
