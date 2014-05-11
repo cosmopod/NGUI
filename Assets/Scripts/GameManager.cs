871f5005652b442d7fbadebae153ce1d393dca0f
cosmopod <fraurerod@gmail.com>
2014-05-11 13:50:26 +0200

    añadido panel de dificultad

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,14 @@ public class GameManager : MonoBehaviour
 {
 
 
+		public enum Difficulties
+		{
+				Easy,
+				Normal,
+				Hard
+		}
+
+		public static Difficulties difficulty = Difficulties.Normal;
 		// Use this for initialization
 		void Start ()
 		{
@@ -21,5 +29,22 @@ public class GameManager : MonoBehaviour
 		{
 				//Exit application
 				Application.Quit ();
+		}
+
+		public void OnDifficultyChange ()
+		{
+				string listValue = UIPopupList.current.value;
+				switch (listValue) {
+				case "Easy":
+						difficulty = Difficulties.Easy;
+						break;
+				case "Normal":
+						difficulty = Difficulties.Normal;
+						break;
+				case "Hard":
+						difficulty = Difficulties.Hard;
+						break;
+				}
+				Debug.Log (difficulty);
 		}
 }
